f13167673155fad1d45d9332528ef84952c219b4
akivagellis <ng73@njit.edu>
2011-06-20 02:55:24 +0000

    midterm

---
+++ index.php
@@ -0,0 +1,95 @@
+ttributes = array();
+
+$tag = '';
+
+$attributes = array('title'=>'my article title', 'body'=>'lorem ipsum');
+
+$content = "lotsastuff";
+
+
+function tag($tag, $attributes, $content) {
+
+ validate$attributes);
+
+        $output = '<' . $tag;
+
+ 
+
+
+        foreach($attributes as $key => $value) {
+
+                        $output .= ' ' . $key . '="' . $value . '"';
+
+        }
+
+ 
+        $output .= '>' . $content . '</' . $tag . '>';
+
+
+
+        echo $output;
+}
+
+ 
+
+
+function validate($value) {
+
+ 
+
+        if(!is_array($value)) {
+
+                echo "ERROR: Your input is not an array!";
+
+        }
+
+}
+
+ 
+
+      function createtag($tag,$attributes,$content){
+
+ 
+
+
+                foreach($attributes as $key=>$value){
+
+                 
+
+                switch($key){
+
+                case'title':
+
+                $tag = 'h1';
+
+                $atributes = array("class"=>"title");
+
+               
+
+                break;
+
+               
+
+                case'body':
+
+                $tag = 'p';
+
+                $atributes = array("class"=>"body");
+
+                break;
+
+                }
+
+                $htmlcontent =tag($tag,$attributes,$content) ;
+
+        }
+
+      }
+
+    create($tag,$attributes,$content);
+
+ 
+
+?>
+
+
